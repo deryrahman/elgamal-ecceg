8f997aa4249c5975a2ae3fffd5ece8fe4e3c3ef4
Daniel <nieltansah@gmail.com>
2019-03-27 04:24:07 +0700

    ecc: curve: 1st impl. of add, mul2, subtract

--- src/ecc/curve.py
+++ src/ecc/curve.py
@@ -1,3 +1,5 @@
+from gmpy2 import divm
+
 from .sqrt import is_square_rootable, square_root
 
 
@@ -18,9 +20,35 @@ class EllipticCurve:
     def __repr__(self):
         return f"(x**3 + {self.a}*x + {self.b}) mod {self.p}"
 
+    def _gradient(self, p, q):
+        dy = p.y - q.y
+        dx = p.x - q.x
+        return divm(dy, dx, self.p)
+
+    def _gradient1(self, p):
+        n = 3 * p.x**2 + self.a
+        d = 2 * p.y
+        return divm(n, d, self.p)
+
+    def _add_g(self, g, p, q):
+        r = Point()
+        r.x = (g**2 - p.x - q.x) % self.p
+        r.y = (g * (p.x - r.x) - p.y) % self.p
+        return r
+
+    def add(self, p, q):
+        return self._add_g(self._gradient(p, q), p, q)
+
+    def substract(self, p, q):
+        q_neg = Point(q.x, -q.y % self.p)
+        return self.add(p, q_neg)
+
+    def mul2(self, p):
+        return self._add_g(self._gradient1(p), p, p)
+
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
