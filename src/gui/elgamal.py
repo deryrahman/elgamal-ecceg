1450420fc3cdd261cd55e5ed7364e8f13547e340
Dery Rahman A <dery.ra@gmail.com>
2019-03-27 06:07:29 +0700

    feat(gui): update elgamal gui

--- src/gui/elgamal.py
+++ src/gui/elgamal.py
@@ -2,9 +2,7 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from elgamal.key_generator import generate_random_prime, generate_public_key
-from elgamal import elgamal
-import utils
+from elgamal import elgamal, utils, key_generator
 import json
 import textwrap
 
@@ -15,7 +13,7 @@ def generate_and_save(button, prime_entry, g_entry, private_key_entry,
     g = int(g_entry.get_text())
     x = int(private_key_entry.get_text())
     try:
-        res = generate_public_key(g, x, p)
+        res = key_generator.generate_public_key(g, x, p)
         with open(save_path.get_text() + '.pub', 'w') as f:
             json.dump(res, f)
         with open(save_path.get_text() + '.pri', 'w') as f:
@@ -31,24 +29,24 @@ def open_key(widget, public_key_entry):
         public_key_entry.set_text(line)
 
 
-def file_select(widget, plain_text):
+def file_select(widget, text, data):
     path = widget.get_filename()
     with open(path, 'rb') as f:
         lines = f.readlines()
-        lines = list(map(lambda x: x.decode("utf-8"), lines))
-        text = ''.join(lines)
-        plain_text.set_text(text)
+        bytes_text = b''.join(lines)
+        data['text'] = bytes_text
+        text.set_text(str(bytes_text))
 
 
 def encrypt(button, public_key_entry, k_entry, plain_text, buffer, data):
     k = int(k_entry.get_text())
     public_key = json.loads(public_key_entry.get_text())
-    plain = utils.to_numpy_array(plain_text.get_text())
+    plain = utils.bytes_to_uint8(data['text'])
     result = elgamal.encrypt(plain, public_key, k)
-    result = utils.int_to_hex(result)
-    data['text'] = result
-    lines = textwrap.wrap(result, 60)
-    if len(lines) > 10:
+    data['save'] = result.tobytes()
+    print(utils.int_to_hex(result))
+    lines = textwrap.wrap(utils.int_to_hex(result), 60)
+    if len(lines) > 20:
         result = 'cipher text to large. use save'
     else:
         result = '\n'.join(lines)
@@ -59,12 +57,13 @@ def decrypt(button, public_key_entry, private_key_entry, cipher_text, buffer,
             data):
     private_key = json.loads(private_key_entry.get_text())
     public_key = json.loads(public_key_entry.get_text())
-    cipher = utils.hex_to_int(cipher_text.get_text())
+
+    cipher = utils.bytes_to_int(data['text'])
     result = elgamal.decrypt(cipher, private_key, public_key['p'])
-    result = utils.to_string(result)
-    data['text'] = result
-    lines = textwrap.wrap(result, 60)
-    if len(lines) > 10:
+    data['save'] = result.tobytes()
+    print(result.tostring())
+    lines = textwrap.wrap(str(result.tostring()), 60)
+    if len(lines) > 20:
         result = 'plain text to large. use save'
     else:
         result = '\n'.join(lines)
@@ -73,8 +72,9 @@ def decrypt(button, public_key_entry, private_key_entry, cipher_text, buffer,
 
 def save_to_file(button, data, save_path):
     path = save_path.get_text()
-    with open(path, 'w') as f:
-        f.write(data['text'])
+    file_bytes = data['save']
+    with open(path, 'wb') as f:
+        f.write(file_bytes)
 
 
 class DialogElgamal(Gtk.Dialog):
@@ -118,7 +118,7 @@ class DialogElgamal(Gtk.Dialog):
         grid.set_row_spacing(10)
 
         prime_info = Gtk.Label("suggested prime number: {}".format(
-            generate_random_prime()))
+            key_generator.generate_random_prime()))
         prime_entry = Gtk.Entry()
         g_entry = Gtk.Entry()
         private_key_entry = Gtk.Entry()
@@ -144,55 +144,6 @@ class DialogElgamal(Gtk.Dialog):
 
         return grid
 
-    def encrypt_page(self):
-        grid = Gtk.Grid(column_homogeneous=True)
-        grid.set_column_spacing(10)
-        grid.set_row_spacing(10)
-
-        public_key_entry = Gtk.Entry()
-        public_key_open = Gtk.FileChooserButton("Open File")
-        public_key_open.set_width_chars(15)
-        public_key_open.connect("selection-changed", open_key, public_key_entry)
-
-        plain_text = Gtk.Entry()
-        buffer = Gtk.TextBuffer()
-        cipher_text = Gtk.TextView(buffer=buffer)
-        cipher_text.set_editable(False)
-
-        file_open = Gtk.FileChooserButton("Open File")
-        file_open.set_width_chars(15)
-        file_open.connect("selection-changed", file_select, plain_text)
-
-        save_path = Gtk.Entry()
-        data = {'text': ""}
-        save_to = Gtk.Button.new_with_label("Save to File")
-        save_to.connect("clicked", save_to_file, data, save_path)
-
-        k_entry = Gtk.Entry()
-
-        button_encrypt = Gtk.Button.new_with_label("Encrypt!")
-        button_encrypt.connect("clicked", encrypt, public_key_entry, k_entry,
-                               plain_text, buffer, data)
-
-        grid.attach(Gtk.Label("Public Key"), 0, 0, 1, 1)
-        grid.attach(public_key_entry, 1, 0, 3, 1)
-        grid.attach(Gtk.Label("Open public key"), 0, 1, 1, 1)
-        grid.attach(public_key_open, 1, 1, 3, 1)
-        grid.attach(Gtk.Label("K value"), 0, 2, 1, 1)
-        grid.attach(k_entry, 1, 2, 3, 1)
-        grid.attach(Gtk.Label("Open file"), 0, 3, 1, 1)
-        grid.attach(file_open, 1, 3, 3, 1)
-        grid.attach(Gtk.Label("Plain text"), 0, 4, 4, 1)
-        grid.attach(plain_text, 0, 5, 4, 1)
-        grid.attach(Gtk.Label("Cipher text"), 0, 6, 4, 1)
-        grid.attach(cipher_text, 0, 7, 4, 1)
-        grid.attach(button_encrypt, 0, 8, 4, 1)
-        grid.attach(Gtk.Label("Save Path"), 0, 9, 1, 1)
-        grid.attach(save_path, 1, 9, 3, 1)
-        grid.attach(save_to, 0, 10, 4, 1)
-
-        return grid
-
     def encrypt_decrypt_page(self, is_encrypt):
         grid = Gtk.Grid(column_homogeneous=True)
         grid.set_column_spacing(10)
@@ -209,17 +160,17 @@ class DialogElgamal(Gtk.Dialog):
         private_key_open.connect("selection-changed", open_key,
                                  private_key_entry)
 
-        plain_text = Gtk.Entry()
+        textbox_upper = Gtk.Entry()
         buffer = Gtk.TextBuffer()
-        cipher_text = Gtk.TextView(buffer=buffer)
-        cipher_text.set_editable(False)
+        textbox_lower = Gtk.TextView(buffer=buffer)
+        textbox_lower.set_editable(False)
 
+        data = {'text': b'', 'save': b''}
         file_open = Gtk.FileChooserButton("Open File")
         file_open.set_width_chars(15)
-        file_open.connect("selection-changed", file_select, plain_text)
+        file_open.connect("selection-changed", file_select, textbox_upper, data)
 
         save_path = Gtk.Entry()
-        data = {'text': ""}
         save_to = Gtk.Button.new_with_label("Save to File")
         save_to.connect("clicked", save_to_file, data, save_path)
 
@@ -227,10 +178,10 @@ class DialogElgamal(Gtk.Dialog):
 
         button_encrypt = Gtk.Button.new_with_label("Encrypt!")
         button_encrypt.connect("clicked", encrypt, public_key_entry, k_entry,
-                               plain_text, buffer, data)
+                               textbox_upper, buffer, data)
         button_decrypt = Gtk.Button.new_with_label("Decrypt!")
         button_decrypt.connect("clicked", decrypt, public_key_entry,
-                               private_key_entry, plain_text, buffer, data)
+                               private_key_entry, textbox_upper, buffer, data)
 
         grid.attach(Gtk.Label("Public Key"), 0, 0, 1, 1)
         grid.attach(public_key_entry, 1, 0, 3, 1)
@@ -243,9 +194,9 @@ class DialogElgamal(Gtk.Dialog):
             grid.attach(Gtk.Label("Open file"), 0, 3, 1, 1)
             grid.attach(file_open, 1, 3, 3, 1)
             grid.attach(Gtk.Label("Plain text"), 0, 4, 4, 1)
-            grid.attach(plain_text, 0, 5, 4, 1)
+            grid.attach(textbox_upper, 0, 5, 4, 1)
             grid.attach(Gtk.Label("Cipher text"), 0, 6, 4, 1)
-            grid.attach(cipher_text, 0, 7, 4, 1)
+            grid.attach(textbox_lower, 0, 7, 4, 1)
             grid.attach(button_encrypt, 0, 8, 4, 1)
             grid.attach(Gtk.Label("Save Path"), 0, 9, 1, 1)
             grid.attach(save_path, 1, 9, 3, 1)
@@ -258,8 +209,11 @@ class DialogElgamal(Gtk.Dialog):
             grid.attach(Gtk.Label("Open file"), 0, 4, 1, 1)
             grid.attach(file_open, 1, 4, 3, 1)
             grid.attach(Gtk.Label("Cipher text"), 0, 5, 4, 1)
-            grid.attach(plain_text, 0, 6, 4, 1)
+            grid.attach(textbox_upper, 0, 6, 4, 1)
             grid.attach(Gtk.Label("Plain text"), 0, 7, 4, 1)
-            grid.attach(cipher_text, 0, 8, 4, 1)
+            grid.attach(textbox_lower, 0, 8, 4, 1)
             grid.attach(button_decrypt, 0, 9, 4, 1)
+            grid.attach(Gtk.Label("Save Path"), 0, 10, 1, 1)
+            grid.attach(save_path, 1, 10, 3, 1)
+            grid.attach(save_to, 0, 11, 4, 1)
         return grid
