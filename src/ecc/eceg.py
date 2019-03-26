fd47632addf23521e4c2674d3555c22c240b9656
Daniel <nieltansah@gmail.com>
2019-03-27 06:40:32 +0700

    ecc: eceg: Simplify gen_pub, just gen. 1 pub. key

--- src/ecc/eceg.py
+++ src/ecc/eceg.py
@@ -3,11 +3,11 @@ from .koblitz import encode1, decode1
 from random import randint
 
 
-def pub_gen(curve, b, pri_a, pri_b):
-    return curve.pow(pri_a, b), curve.pow(pri_b, b)
+def gen_pub(curve, b, pri):
+    return curve.pow(pri, b)
 
 
-def encrypt(curve, b, pub_b, koblitz_k, msg):
+def encrypt(curve, b, pub, koblitz_k, msg):
     out = []
     for m in msg:
         pm = encode1(curve, koblitz_k, m)
@@ -16,15 +16,15 @@ def encrypt(curve, b, pub_b, koblitz_k, msg):
 
         out.append((
             curve.pow(k, b),
-            curve.add(pm, curve.pow(k, pub_b))))
+            curve.add(pm, curve.pow(k, pub))))
     return out
 
 
-def decrypt(curve, b, pri_b, koblitz_k, cipher_msg):
+def decrypt(curve, b, pri, koblitz_k, cipher_msg):
     out = []
     for msg_tuple in cipher_msg:
         m_a, m_b = msg_tuple
-        m = curve.subtract(m_b, curve.pow(pri_b, m_a))
+        m = curve.subtract(m_b, curve.pow(pri, m_a))
         print(m)
         out.append(decode1(koblitz_k, m))
     return bytes(out)
